a9142de7727b1595436dab972d8f74839c5aab9d
Pimpakarn26 <bonemarrow26bp@gmail.com>
2023-11-05 22:54:10 +0700

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+export {default as AddStudent} from"./AddStudent"
+export { default as EditStudent } from "./EditStudent";
+export { default as StadentDetail} from "./StadentDetail";
+export { default as StudentList } from "./StudentList";
